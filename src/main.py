a3c01eb4901a17134a08198460b2b5a2d563bb80
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-06-10 17:32:17 -0400

    feat: improved hashing and performance Just removing the extra file download has cut time in half. Pretty insane performance jump.

--- src/main.py
+++ src/main.py
@@ -1,18 +1,20 @@
+import hashlib
 import json
 import sys
+import subprocess
 
 from concurrent.futures import ThreadPoolExecutor
 from io import StringIO, BytesIO
+from multiprocessing import cpu_count
 from shutil import which
-from subprocess import run
 from urllib.request import urlopen
 from zipfile import ZipFile
 
 BIN_VSCODE = which("code")
-BIN_PREFETCH = which("nix-prefetch-url")
+BIN_HASH = which("nix-hash")
 
 def run_cmd(bin, *args):
-    return run([ bin ] + list(args), capture_output=True, text=True).stdout.splitlines()
+    return subprocess.run([ bin ] + list(args), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True).stdout.splitlines()
 
 def parse_ext(args):
     ext, buff = args
@@ -22,19 +24,17 @@ def parse_ext(args):
 
     print(f"\033[KDownloading {ext}...", end="\r", file=sys.stderr)
 
-    with ZipFile(BytesIO(urlopen(url_vsix).read())) as z:
-        with z.open("extension/package.json") as f:
-            version = json.load(f)["version"]
-            url_hash = f"https://marketplace.visualstudio.com/_apis/public/gallery/publishers/{publisher}/vsextensions/{name}/{version}/vspackage"
-            sha256 = run_cmd(BIN_PREFETCH, url_hash)[0]
+    with urlopen(url_vsix) as resp, BytesIO(resp.read()) as fp, ZipFile(fp) as z, z.open("extension/package.json") as f:
+        version = json.load(f)["version"]
+        sha256 = run_cmd(BIN_HASH, "--to-base32", "--type", "sha256", hashlib.sha256(fp.read()).hexdigest())[0]
 
-            # Normally this would be a concurrency issue but the GIL takes care of this for us
-            buff.write("  {\n" +
-                      f"    name = \"{name}\";\n" +
-                      f"    publisher = \"{publisher}\";\n" +
-                      f"    version = \"{version}\";\n" +
-                      f"    sha256 = \"{sha256}\";\n" +
-                       "  }\n")
+        # Normally this would be a concurrency issue but the GIL takes care of this for us
+        buff.write("  {\n" +
+                  f"    name = \"{name}\";\n" +
+                  f"    publisher = \"{publisher}\";\n" +
+                  f"    version = \"{version}\";\n" +
+                  f"    sha256 = \"{sha256}\";\n" +
+                   "  }\n")
 
 def main():
     with StringIO("") as buff:
@@ -42,12 +42,12 @@ def main():
 
         buff.write("[\n")
 
-        with ThreadPoolExecutor(max_workers=len(exts)) as executor:
+        with ThreadPoolExecutor(max_workers=cpu_count() * 2) as executor:
             executor.map(parse_ext, exts)
 
         buff.write("]")
 
-        print("\033[K", end="\r")
+        print("\r\033[K", end="\r", file=sys.stderr)
         print(buff.getvalue())
 
 if __name__ == "__main__":
