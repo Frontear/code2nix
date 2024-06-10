1396849a1493ef2993595ee746bfb82101dde01f
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-06-09 20:19:51 -0400

    feat: some partial attempt at parallelization Seeing yuce.erlang-otp taking a long time, and in general I think its wasteful to download the whole VSIX for just the package.json. Going to try and look for another way.

--- src/main.py
+++ src/main.py
@@ -1,36 +1,68 @@
 import json
 import sys
-import shutil
 import subprocess
 
-from io import BytesIO
+from concurrent.futures import ThreadPoolExecutor
+from io import StringIO, BytesIO
+from multiprocessing import cpu_count as cores
+from shutil import which
 from urllib.request import urlopen
 from zipfile import ZipFile
 
+BIN_VSCODE = which("code")
+BIN_PREFETCH = which("nix-prefetch-url")
+
+def run_cmd(bin, *args):
+    return subprocess.run([ bin ] + list(args), capture_output=True, text=True).stdout.splitlines()
+
+def parse_ext(ext):
+    publisher, name = ext.split(".")
+    url_vsix = f"https://{publisher}.gallery.vsassets.io/_apis/public/gallery/publisher/{publisher}/extension/{name}/latest/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
+
+    print(f"\033[KDownloading {ext}...", end="\r", file=sys.stderr)
+
+    with ZipFile(BytesIO(urlopen(url_vsix).read())) as z:
+        with z.open("extension/package.json") as f:
+            version = json.load(f)["version"]
+            url_hash = f"https://marketplace.visualstudio.com/_apis/public/gallery/publishers/{publisher}/vsextensions/{name}/{version}/vspackage"
+            sha256 = run_cmd(BIN_PREFETCH, url_hash)
+
+            return {
+                "name": name,
+                "publisher": publisher,
+                "version": version,
+                "sha256": sha256[0]
+            }
+
+def main():
+    exts = run_cmd(BIN_VSCODE, "--list-extensions")
+    tasks = []
+
+    with ThreadPoolExecutor(max_workers=cores()) as executor:
+        for ext in exts:
+            tasks.append(executor.submit(parse_ext, ext))
+
+        with StringIO("") as s:
+            s.write("[\n")
+
+            for task in tasks:
+                if not task.done():
+                    tasks.append(task)
+                    continue
+
+                attr = task.result()
+
+                s.write("  {\n")
+                s.write(f"    name = \"{attr['name']}\";\n")
+                s.write(f"    publisher = \"{attr['publisher']}\";\n")
+                s.write(f"    version = \"{attr['version']}\";\n")
+                s.write(f"    sha256 = \"{attr['sha256']}\";\n")
+                s.write("  }\n")
+
+            s.write("]")
+
+            print("\033[K")
+            print(s.getvalue())
+
 if __name__ == "__main__":
-    code_bin = shutil.which("code")
-    prefetch_bin = shutil.which("nix-prefetch-url")
-
-    extensions = subprocess.run([ code_bin, "--list-extensions" ], capture_output=True, text=True).stdout.splitlines()
-
-    ext_list = []
-
-    for n, ext in enumerate(extensions):
-        pub, name = ext.split(".")
-
-        # https://stackoverflow.com/a/31417845/9091276
-        print(f"\033[KDownloading {ext}... ({n}/{len(extensions)})", end="\r", file=sys.stderr)
-
-        with ZipFile(BytesIO(urlopen(f"https://{pub}.gallery.vsassets.io/_apis/public/gallery/publisher/{pub}/extension/{name}/latest/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage").read())) as z:
-            with z.open("extension/package.json") as f:
-                ver = json.load(f)["version"]
-                sha256 = subprocess.run([ prefetch_bin, f"https://marketplace.visualstudio.com/_apis/public/gallery/publishers/{pub}/vsextensions/{name}/{ver}/vspackage" ], capture_output=True, text=True).stdout.splitlines()[0]
-
-                ext_list.append({
-                    "name": name,
-                    "publisher": pub,
-                    "version": ver,
-                    "sha256": sha256
-                })
-
-    print(json.dumps(ext_list))
+    main()
