7aa5490488d76bffdfa8055747f468751a7afb53
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-07-22 13:00:03 -0400

    major: clean slate Yes.. I know

--- nix/default.nix
+++
@@ -1,7 +0,0 @@
-{ ... }: {
-  perSystem = { pkgs, ... }: {
-    devShells.default = pkgs.callPackage ./shell.nix { };
-
-    packages.default = pkgs.python3Packages.callPackage ./package.nix { };
-  };
-}
