60108899d6ee9591ea42f12092340fc23cd2c3c3
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-07-22 15:11:16 -0400

    test.sh: cleanup script Still fails but its a bit cleaner, wanted to clean up before continuing

--- test.sh
+++ test.sh
@@ -1,65 +1,78 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -i bash -p curl jq nix unzip 
 
-name="remote-ssh-edit"
-pub="ms-vscode-remote"
-ver="0.47.2"
-sha=""
-
-function get_current() {
+function extension() {
     local pub="$1"
     local name="$2"
-    local ver="$3"
+    local ver="${3:-latest}"
     local hash=""
 
-    local hash=$(nix-prefetch-url "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/${pub}/vsextensions/${name}/${ver}/vspackage" 2> /dev/null)
+    local tmp="$(mktemp -d)"
+    local file="$tmp/extension.zip"
 
-    cat <<- EOF
-  {
-      name = "${name}";
-      publisher = "${pub}";
-      version = "${ver}";
-      sha256 = "${hash}";
-  }
-EOF
-}
-
-function get_latest() {
-    local pub="$1"
-    local name="$2"
-    local ver=""
-    local hash=""
-
-    local tmp=$(mktemp -d)
-    local file="$tmp/latest.zip"
-
-    curl --silent -o "$file" "https://${pub}.gallery.vsassets.io/_apis/public/gallery/publisher/${pub}/extension/${name}/latest/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
+    curl --silent -o "$file" "https://${pub}.gallery.vsassets.io/_apis/public/gallery/publisher/${pub}/extension/${name}/${ver}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
     
-    ver=$(jq -r '.version' <(unzip -qc "$file" "extension/package.json"))
-    hash=$(nix-hash --flat --base32 --type sha256 "$file" 2> /dev/null)
+    if [ "$ver" = "latest" ]; then
+        local ver=$(jq -r '.version' <(unzip -qc "$file" "extension/package.json"))
+    fi
+
+    local hash=$(nix-hash --flat --base32 --type sha256 "$file" 2> /dev/null)
 
     rm -rf "$tmp"
 
-    cat <<-EOF
+    cat <<- EOF
   {
-      name = "${name}";
-      publisher = "${pub}";
-      version = "${ver}";
-      sha256 = "${hash}";
+    name = "${name}";
+    publisher = "${pub}";
+    version = "${ver}";
+    sha256 = "${hash}";
   }
 EOF
 }
 
-echo "["
+function generate_expressions() {
+    local type="$1" # One of: latest, current
+    local ext_nix="${type}/extensions.nix"
+    local pkg_nix="${type}/package.nix"
 
-#for i in $(code --list-extensions); do
-for i in $(code --list-extensions --show-versions); do
-    VER=$(echo "$i" | cut -d@ -f2)
-    NAME=$(echo "$i" | cut -d@ -f1 | cut -d. -f1)
-    PUB=$(echo "$i" | cut -d@ -f1 | cut -d. -f2)
+    local args="--list-extensions"
 
-    get_current "$NAME" "$PUB" "$VER"
-    #get_latest "$NAME" "$PUB"
-done
+    if [ "$type" = "current" ]; then
+        args+=" --show-versions"
+    fi
 
-echo "]"
+    mkdir -p "${type}"
+
+    echo "[" > "$ext_nix"
+
+    for i in $(code $args); do
+        if [ "$type" = "latest" ]; then
+            extension "$(echo "$i" | cut -d@ -f1 | cut -d. -f1)" "$(echo "$i" | cut -d@ -f1 | cut -d. -f2)" >> "$ext_nix"
+        else
+            extension "$(echo "$i" | cut -d@ -f1 | cut -d. -f1)" "$(echo "$i" | cut -d@ -f1 | cut -d. -f2)" "$(echo "$i" | cut -d@ -f2)" >> "$ext_nix"
+        fi
+    done
+    echo "]" >> "$ext_nix"
+
+    cat << EOF > "$pkg_nix"
+{
+  pkgs ? import <nixpkgs> { config.allowUnfree = true; }
+}:
+pkgs.vscode-with-extensions.override {
+  vscodeExtensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace (import ./$(basename "$ext_nix");
+}
+EOF
+
+    echo "$pkg_nix"
+}
+
+if ! which code; then
+    echo "VSCode not found in PATH, cannot continue"
+    exit 1
+fi
+
+f1=$(generate_expressions "current")
+f2=$(generate_expressions "latest")
+
+nix-build --quiet --no-out-link "$f1"
+nix-build --quiet --no-out-link "$f2"
