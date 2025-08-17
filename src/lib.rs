ad609c0d1ba0124304a80c7b48548c10f290282b
Ali Rizvi <contact@frontear.dev>
2025-08-16 23:36:15 -0400

    feat: partially implement proper support for `targetPlatform` payload
    This is such a huge step in fixing the god-awful inconsistencies of the normal URL.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
+mod args;
+mod extension;
+
 use std::process::Command;
 
-use serde::{
-  Deserialize,
-  Serialize
+pub use args::{
+  CliArgs,
+  ExtSource,
 };
+
+pub use extension::Extension;
 
 pub fn run_cmd(binary: &str, args: Vec<&str>) -> String {
   let stdout = Command::new(binary)
@@ -12,30 +17,4 @@ pub fn run_cmd(binary: &str, args: Vec<&str>) -> String {
     .expect(format!("Failed to run '{binary}' with args: '{:?}'", &args).as_str());
 
   return String::from_utf8(stdout.stdout).unwrap();
-}
-
-#[derive(Deserialize, Serialize)]
-pub struct Extension {
-  pub publisher: String,
-  pub name: String,
-  pub version: String,
-  pub hash: Option<String>,
-}
-
-impl Extension {
-  pub fn from_code(line: &str) -> Self {
-    let (publisher, name_version) = line.split_once('.').unwrap();
-    let (name, version) = name_version.split_once('@').unwrap();
-
-    return Extension {
-      publisher: publisher.to_owned(),
-      name: name.to_owned(),
-      version: version.to_owned(),
-      hash: None,
-    };
-  }
-
-  pub fn url(&self) -> String {
-    return format!("https://{0}.gallery.vsassets.io/_apis/public/gallery/publisher/{0}/extension/{1}/{2}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage", self.publisher, self.name, self.version);
-  }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use std::error::Error;
+use std::io::Cursor;
+use std::io::Read;
+use std::path::PathBuf;
 
+use clap::Parser;
 use reqwest::blocking::Client;
+use serde_json::Value;
 use sha256;
 use tempfile::NamedTempFile;
+use zip::ZipArchive;
 
 use code2nix::*;
 
@@ -13,7 +19,30 @@ fn from_code() -> Vec<Extension> {
   let lines = out.lines();
 
   for line in lines {
-    exts.push(Extension::from_code(line));
+    let (publisher, name_version) = line.split_once('.').unwrap();
+    let (name, version) = name_version.split_once('@').unwrap();
+
+    exts.push(Extension {
+      publisher: publisher.to_owned(),
+      name: name.to_owned(),
+      version: Some(version.to_owned()),
+      arch: None,
+      hash: None,
+    });
+  }
+
+  return exts;
+}
+
+fn from_file(file: &PathBuf) -> Vec<Extension> {
+  let mut exts: Vec<Extension>;
+
+  let out = run_cmd("nix", vec![ "eval", "--json", "--file", file.to_str().unwrap() ]);
+  exts = serde_json::from_str(out.as_str()).unwrap();
+
+  for ext in exts.iter_mut() {
+    // forcefully resolve the latest version
+    ext.version = None;
   }
 
   return exts;
@@ -23,9 +52,34 @@ fn download_ext(client: &Client, ext: &mut Extension) -> Result<(), Box<dyn Erro
   let resp = client
     .get(ext.url())
     .send()
-    .expect(format!("Failed to download extension ({}.{}@{}).", ext.publisher, ext.name, ext.version).as_str());
+    .expect(format!("Failed to download extension ({}.{}).", ext.publisher, ext.name).as_str())
+    .bytes()?;
 
-  let digest = sha256::digest(resp.bytes()?.as_ref());
+  if ext.version == None || ext.arch == None {
+    let mut archive = ZipArchive::new(Cursor::new(resp.as_ref()))?;
+    let json: Value = serde_json::from_reader(archive.by_name("extension/package.json")?)?;
+
+    ext.version = Some(json.as_object().unwrap()["version"].as_str().unwrap().to_owned());
+
+    let mut content = String::new();
+    archive.by_name("extension.vsixmanifest")?.read_to_string(&mut content)?;
+
+    if content.contains("TargetPlatform") {
+      // TODO: this is hardcoded to 'linux-x64' since that's all I use
+      ext.arch = Some("linux-x64".to_owned());
+    }
+    else {
+      ext.arch = Some("".to_owned());
+    }
+
+    // force a re-download with the new version and architecture fields
+    // to obtain the correct extension zip for hashing.
+    //
+    // See https://github.com/Frontear/code2nix/issues/2 for details.
+    return download_ext(client, ext);
+  }
+
+  let digest = sha256::digest(resp.as_ref());
   let out = run_cmd("nix", vec![ "hash", "to-sri", "--type", "sha256", &digest ]);
 
   ext.hash = Some(out);
@@ -34,16 +88,20 @@ fn download_ext(client: &Client, ext: &mut Extension) -> Result<(), Box<dyn Erro
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-  let client = Client::new();
-  let mut exts = from_code();
+  let args = CliArgs::parse();
+  let mut exts = match &args.command {
+    ExtSource::Code => from_code(),
+    ExtSource::File(args) => from_file(&args.file),
+  };
 
-  for mut ext in exts.iter_mut() {
-    download_ext(&client, &mut ext)?;
+  let client = Client::new();
+  for ext in exts.iter_mut() {
+    download_ext(&client, ext)?;
   }
 
   let file = NamedTempFile::new()?;
   serde_json::to_writer(&file, &exts)?;
-  let out = run_cmd("nix", vec![ "eval", "--impure", "--expr", format!("builtins.fromJSON (builtins.readFile {})", file.path().to_str().unwrap()).as_str() ]);
+  let out = run_cmd("nix", vec![ "eval", "--impure", "--expr", format!("builtins.fromJSON (builtins.readFile {})", &file.path().to_str().unwrap()).as_str() ]);
 
   println!("{}", out);
 
